1f4642ae211f7db80b873296390edbd2f5aef1b6
小白-白 <xb@fczbl.vip>
2018-06-15 18:10:04 +0800

    First Commit.

---
+++ statics/js/main.js
@@ -0,0 +1,83 @@
+$('.menu a').click(function(){
+    target = $(this).attr('goto');
+    switchTo(target);
+    $('.menu li a').each(function(){
+        $(this).removeClass('active');
+    });
+    $(this).addClass('active');
+});
+
+function switchTo(target){
+    $('.right section').each(function () {
+        $(this).removeClass('active');
+    });
+    $(target).addClass('active');
+}
+
+function getAchives(){
+    t = ``;
+    $.ajax({
+        type:"GET",
+        url:"https://www.fczbl.vip/wp-json/wp/v2/posts?per_page=8&page=1",
+        dataType:"json",
+        success:function(json){
+            for(var i = 0;i < json.length;i++){
+                title = json[i].title.rendered;
+                link = json[i].link;
+                time = new Date(json[i].date).Format("yyyy-MM-dd");
+                t += `<li><a href="${link}" target="_blank">${title} <span class="meta">/ ${time}</span></a></li>`;
+                $('.archive-list').html(t);
+            }
+        }
+    })
+}
+
+function gethitokoto(){
+    $.ajax({
+        type:"POST",
+        url:"https://www.fczbl.vip/api/hitokoto/?encode=json",
+        dataType:"json",
+        success:function(result){
+            write(result.hitokoto);
+        },
+        error:function(){
+            write("Error: Failed to get hitokoto.");
+        }
+    });
+}
+
+function write(text){
+    if (text.length < 25) {
+        $('#hitokoto').html(text);
+    } else {
+        gethitokoto();
+    }
+}
+
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+// 例子： 
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
+Date.prototype.Format = function(fmt){ //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if(/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1,(this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+$(document).ready(function(){
+    getAchives();
+    gethitokoto();
+    setTimeout(function(){$(".loading").hide();},1500);
+});
